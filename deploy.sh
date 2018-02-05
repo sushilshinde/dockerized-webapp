3032fc4c721e85055d64d24409111006bb823423
Sushil Shinde <shinde.sushil@gmail.com>
2018-02-05 16:09:35 +0530

    f

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ TAG=$2
 configure_aws_cli() {
 	aws --version
 	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+	echo $AWS_ACCESS_KEY_ID
 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 	aws configure set default.region us-east-1
 	aws configure set default.output json
